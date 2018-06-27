59a8e5f0ea2e89c736df99a4906c5e683bcd53b5
Prasanna <prasannaraajkumar@gmail.com>
2018-06-26 23:25:10 -0700

    express 2 added

---
+++ nodejs/express_2.js
@@ -0,0 +1,15 @@
+var request = require('request');
+const express = require('express');
+
+const app = express();
+
+app.get('/request', function(req, res) {
+	request('http://google.com', function(error, response, body) {
+            res.status(200).send(body);
+    });
+});
+
+const port = 3000;
+app.listen(port, function() {
+	console.log('server running at port ' + port);
+});
