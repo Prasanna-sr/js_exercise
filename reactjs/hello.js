591f8973aec12171b3c05889e577843703c7433d
Prasanna <prasannaraajkumar@gmail.com>
2018-06-26 21:45:18 -0700

    react component added

--- reactjs/hello.js
+++ reactjs/hello.js
@@ -1,5 +1,24 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-const element = <h1>Hello, world</h1>;
+
+// 1 React elements
+
+const name = 'John';
+const element = <h1>Hello, {name}</h1>;
+
 ReactDOM.render(element, document.getElementById('root'));
+
+
+// 2 React Components
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+//
+//
+// ReactDOM.render(
+//   <Welcome name='Prasanna' />,
+//   document.getElementById('root')
+// );
